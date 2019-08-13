834a0b05f4c500bb8ad5ad2b51a3fbb2a13ca820
sahoI <saho0003@gmail.com>
2019-08-13 21:23:16 +0900

    テストデータの表示

---
+++ resources/views/stores/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+@section('content')
+    <div class="containew mt-4">
+        @foreach ($stores as $store)
+            <div class="card mb-4">
+                <div class="card-header">
+                    {{ $store->name }}
+                </div>
+                <div class="card-body">
+                    {{--<p class="card-text">--}}
+                        {{--{!! nl2br(e(str_limit($store->body, 200))) !!}--}}
+                    {{--</p>--}}
+                </div>
+                <div class="card-footer">
+                    <span class="mr-2">
+                        登録日時 {{ $store->created_at->format('Y.m.d') }}
+                    </span>
+
+                    @if ($store->items->count())
+                        <span class="badge badge-primary">
+                            商品数 {{ $store->items->count() }}件
+                        </span>
+                    @endif
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
